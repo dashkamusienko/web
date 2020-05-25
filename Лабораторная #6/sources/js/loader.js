82b3b65c936bb624dc59814d92d4eb01deb58459
dashkamusienko <65846207+dashkamusienko@users.noreply.github.com>
2020-05-25 17:08:54 +0300

    Add files via upload

---
+++ Лабораторная #6/sources/js/loader.js
@@ -0,0 +1,6 @@
+function loader () {
+  setTimeout(function () {
+    document.getElementById("loader").style.visibility = "hidden";
+    document.getElementById("blank").style.visibility = "hidden";
+  }, 2000)
+}
